e9ace9c9977ebfa3969c099d6932480ed5820d8d
fromm <fromm@DESKTOP-OGOPN3J>
2023-02-08 22:04:01 +0100

    Initial Release

---
+++ ScrollWriter/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace ScrollWriter
+{
+	public class ScrollWriter
+	{
+		public void Write(string text, int waitPeriod = 50)
+		{
+			foreach (char character in text)
+			{
+				Console.Write(character);
+				Thread.Sleep(waitPeriod);
+			}
+		}
+		
+		public void WriteLine(string text, int waitPeriod = 50)
+		{
+			text += "\n";
+			foreach (char character in text)
+			{
+				Console.Write(character);
+				Thread.Sleep(waitPeriod);
+			}
+		}
+		public void WriteVertical(string text, int waitPeriod = 50)
+		{
+			foreach (char character in text)
+			{
+				Console.WriteLine(character);
+				Thread.Sleep(waitPeriod);
+			}
+		}
+	}
+}
